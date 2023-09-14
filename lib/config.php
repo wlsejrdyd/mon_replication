ce0f9bcaa1767f5bcd89045c40166961c47ddf28
찐떡 <130738855+wlsejrdyd@users.noreply.github.com>
2023-09-14 17:07:26 +0900

    Add files via upload

---
+++ lib/config.php
@@ -0,0 +1,9 @@
+<?
+$Host = "localhost";
+$User = "deok";
+$Pass = "deok00##";
+$DB = "deok";
+
+$con = mysqli_connect($Host,$User,$Pass,$DB) or die('Failed');
+#$result = mysqli_select_db($DB, $con);
+?>
